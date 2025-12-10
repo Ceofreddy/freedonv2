05ae62c1e331c24e793e25caa5397165f3fbc941
Mesoflix-DEVOPS <projectmesoflix@gmail.com>
2025-12-10 13:28:51 +0300

    Update main.tsx

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -340,7 +340,7 @@ const AppWrapper = observer(() => {
     const fetchBots = async () => {
       const botFiles = [
         "ODD_Autobot.xml",
-        "ODD_Autobot (1).xml",
+        "EVEN_Autobot.xml",
         "OVER_UNDER AUTOBOT.xml",
         "Over 3 strategybot.xml",
         "EVEN_MYTH V2.0 .xml",
